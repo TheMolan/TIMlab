adcd0b27c5516ea344889b24c67ac2d1a201a8cf
TheMolan <55600816+TheMolan@users.noreply.github.com>
2020-02-03 02:23:54 +0300

    Delete source.py

--- source.py
+++
@@ -1,8 +0,0 @@
-from request_receiver_class import *
-from executor_class import *
-from server_repository_class import *
-from server_log_repository_class import *
-from info_sender_class import *
-
-RR = request_receiver()
-RR.receive()
